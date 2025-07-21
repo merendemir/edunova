dda5f42160bba0aedc6bc6986e231a53f2e9c73f
eren.demir <eren.demir@iyzico.com>
2025-07-21 17:02:52 +0300

    remove unused ResponseHandler call from DiscoveryServerApplication

--- discovery-server/src/main/java/com/edunova/commons/discover/server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/edunova/commons/discover/server/DiscoveryServerApplication.java
@@ -1,6 +1,5 @@
 package com.edunova.commons.discover.server;
 
-import com.edunova.commons.api.ResponseHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,6 +9,5 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
-        ResponseHandler.success();
     }
 }
