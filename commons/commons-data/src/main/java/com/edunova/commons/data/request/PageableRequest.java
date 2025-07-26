b91b1f6db5fbaf5309e556ef82a0895188ebc946
eren.demir <eren.demir@iyzico.com>
2025-07-26 22:24:57 +0300

    refactor: update sorting logic in PageableRequest to handle single string sort parameters

--- commons/commons-data/src/main/java/com/edunova/commons/data/request/PageableRequest.java
+++ commons/commons-data/src/main/java/com/edunova/commons/data/request/PageableRequest.java
@@ -18,11 +18,12 @@ public class PageableRequest {
     @Min(value = 1, message = "{page.request.size.min}")
     private int size = 10;
     
-    private List<String[]> sort;
+    private List<String> sort;
     
     public Pageable toPageable() {
         Sort sort = Sort.unsorted();
         if (this.sort != null && !this.sort.isEmpty()) {
+            this.sort = getSortList(this.sort);
             Sort.Order[] orders = this.sort.stream()
                 .map(this::parseSort)
                 .toArray(Sort.Order[]::new);
@@ -30,8 +31,20 @@ public class PageableRequest {
         }
         return PageRequest.of(page, size, sort);
     }
+
+    private List<String> getSortList(List<String> sort) {
+        if (sort.size() == 2) {
+            String secondElement = sort.get(1).toLowerCase();
+            if ("asc".equals(secondElement) || "desc".equals(secondElement)) {
+                return List.of(sort.get(0) + "," + sort.get(1));
+            }
+        }
+
+        return sort;
+    }
     
-    private Sort.Order parseSort(String[] sortArray) {
+    private Sort.Order parseSort(String sort) {
+        String[] sortArray = sort.split(",");
         String property = sortArray[0].trim();
         Sort.Direction direction = sortArray.length > 1 && "desc".equalsIgnoreCase(sortArray[1].trim())
             ? Sort.Direction.DESC
